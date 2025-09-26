51aee346f17a15fe59d6e6bfc88f4058bcaf7721
KorolevaAnastasia <aselona@ya.ru>
2025-09-26 17:58:50 +0300

    +

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
